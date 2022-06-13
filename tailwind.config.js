f8c923128d18e5675c9ed78c8e1a4800cacb4802
john-mirage <chaosdynamix@posteo.net>
2022-06-13 17:10:34 +0200

    feat: initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,78 @@
+/** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme');
+
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    screens: {
+      "xs": "375px",
+      "sm": "576px",
+      "md": "768px",
+      "lg": "992px",
+      "xl": "1200px",
+      "2xl": "1400px",
+    },
+    colors: {
+      "transparent": "transparent",
+      "bright-orange": "hsl(31, 77%, 52%)",
+      "dark-cyan": "hsl(184, 100%, 22%)",
+      "very-dark-cyan": "hsl(179, 100%, 13%)",
+      "transparent-white": "hsla(0, 0%, 100%, 0.75)",
+      "very-light-gray": "hsl(0, 0%, 95%)",
+    },
+    spacing: {
+      "0": "0px",
+      "1": "0.0625rem",
+      "2": "0.125rem",
+      "4": "0.25rem",
+      "8": "0.5rem",
+      "12": "0.75rem",
+      "16": "1rem",
+      "20": "1.25rem",
+      "24": "1.5rem",
+      "28": "1.75rem",
+      "32": "2rem",
+      "36": "2.25rem",
+      "40": "2.5rem",
+      "44": "2.75rem",
+      "48": "3rem",
+      "52": "3.25rem",
+      "56": "3.5rem",
+      "60": "3.75rem",
+      "64": "4rem",
+      "68": "4.25rem",
+      "72": "4.5rem",
+      "76": "4.75rem",
+      "80": "5rem",
+      "84": "5.25rem",
+      "88": "5.5rem",
+      "92": "5.75rem",
+      "96": "6rem",
+      "100": "6.25rem",
+      "128": "8rem",
+      "256": "16rem",
+      "512": "32rem",
+      "1024": "64rem",
+    },
+    fontWeight: {
+      "400": "400",
+      "700": "700",
+    },
+    extend: {
+      fontFamily: {
+        heading: [
+          'Big Shoulders Display',
+          ...defaultTheme.fontFamily.sans,
+        ],
+        body: [
+          "Lexend Deca",
+          ...defaultTheme.fontFamily.sans,
+        ],
+      },
+    },
+  },
+  plugins: [],
+}
