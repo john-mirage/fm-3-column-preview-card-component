f6b686e507fbacf753896391119b0ffe6a31741e
John Mirage <28307090+john-mirage@users.noreply.github.com>
2023-05-20 13:25:20 +0200

    fix: base path

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import { resolve } from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/3-column-preview-card-component/",
   resolve: {
     alias: {
       '@images': resolve(__dirname, 'src/images'),
